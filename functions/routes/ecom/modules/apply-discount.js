82984ab3bf8673d551c5350305bd1a1a9ad0c4e0
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-07-29 11:06:32 -0300

    chore: test with removing only freebie

--- functions/routes/ecom/modules/apply-discount.js
+++ functions/routes/ecom/modules/apply-discount.js
@@ -168,7 +168,7 @@ exports.post = ({ appSdk, admin }, req, res) => {
           let value = 0
           rule.product_ids.forEach(productId => {
             const item = params.items.find(item => productId === item.product_id)
-            if (item) {
+            if (item && item.flags && item.flags.includes('freebie')) {
               value += ecomUtils.price(item)
             }
           })
